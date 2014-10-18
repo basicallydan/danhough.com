a0fd9120bfdb0942a2aa1bc82889c0919e4962f1
Dan Hough <daniel.hough@gmail.com>
2014-10-18 14:19:30 +0100

    Moved images back

--- _plugins/rsync_image_generator.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll    
-  class RsyncImageGenerator < Generator
-    def generate(site)
-      system('mkdir -p _site');
-      system('rsync --archive --delete _img/ _site/img/');
-    end
-  end
-end  
