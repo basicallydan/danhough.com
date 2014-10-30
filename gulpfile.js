6a1c3424d17e87c483b52ad888f118be50cc1a84
Dan Hough <daniel.hough@gmail.com>
2014-10-30 13:45:25 +0000

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var config = {
 		destination: './_site'
 	},
 	live: {
-		destination: '/home/dan/websites/danielhough.co.uk/www'
+		destination: '/home/dan/websites/danhough.com/www'
 	}
 };
 var environment = 'local';
